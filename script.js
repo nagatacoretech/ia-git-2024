07a1e395206a78403c628018a127ced357037840
nagatacoretech <t.nagata@core-tech.jp>
2024-05-21 15:52:29 +0900

    hotfixにて緊急修正

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.getElementById('btn1').addEventListener('click', function() {
     alert('エラーです。直してください');
     // 以下のコメントを解除すればOK
-    // document.getElementById('p1').classList.add('color1');
+    document.getElementById('p1').classList.add('color1');
     });
