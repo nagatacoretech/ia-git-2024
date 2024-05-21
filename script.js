d2272534e255b88dbdfcea28d535a5a88189ab61
nagatacoretech <t.nagata@core-tech.jp>
2024-05-21 15:34:07 +0900

    エラーが発覚します。次のコミットで直します

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('btn1').addEventListener('click', function() {
+    alert('エラーです。直してください');
+    // 以下のコメントを解除すればOK
+    // document.getElementById('p1').classList.add('color1');
+    });
